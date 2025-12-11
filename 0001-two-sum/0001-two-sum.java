0c9be8e02f8d540da594e85e421e998d2a8c076f
DOminator_04 <adipatil560@gmail.com>
2025-12-11 14:04:17 +0530

    Time: 45 ms (28.96%), Space: 46.4 MB (41.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int sum = nums[i] + nums[j]; // sum values, not indices
-                if (sum == target) {
-                    return new int[]{i, j}; // return indices
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                int sum= nums[i]+nums[j];
+                if(sum==target){
+                    return new int[]{i,j};
                 }
             }
         }
-        return new int[]{}; // if no pair found
+        return new int[]{};
+        
     }
 }
