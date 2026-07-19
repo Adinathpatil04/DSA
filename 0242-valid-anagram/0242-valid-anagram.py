9622a5bdbb32cce6366b674c3e941d2ae2f07c17
Adinath_04 <adipatil560@gmail.com>
2026-07-19 14:40:34 +0530

    Time: 27 ms (23.52%), Space: 14.6 MB (8.08%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = list(s)
+        t = list(t)
+
+        s.sort()
+        t.sort()
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+
+        return True
