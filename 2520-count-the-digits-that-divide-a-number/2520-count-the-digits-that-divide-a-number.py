0f503bf296188d907b381b38aa2b1a085bacc760
Adinath_04 <adipatil560@gmail.com>
2026-07-26 23:30:14 +0530

    Time: 0 ms (100%), Space: 12.3 MB (88.25%) - LeetHub

---
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countDigits(self, num):
+        original = num
+        count = 0
+
+        while num > 0:
+            digit = num % 10
+
+            if original % digit == 0:
+                count += 1
+
+            num //= 10
+
+        return count
