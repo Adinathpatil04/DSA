56669574f789b6ddb2c14a9b849c59190215a087
Adinath_04 <adipatil560@gmail.com>
2026-08-03 18:57:31 +0530

    Time: 11 ms (7.49%), Space: 12.4 MB (19.79%) - LeetHub

---
+++ 3046-split-the-array/3046-split-the-array.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPossibleToSplit(self, nums):
+        h={}
+
+        for i in nums:
+            if i in h.keys() and h[i]==2:
+                return False
+
+            elif i in h.keys():
+                h[i]=h[i]+1
+
+            else:
+                h[i]=1
+        return True
+
+
+        
